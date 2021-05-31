db12210959a97aaa8636d096eb11ac47279c10eb
Thomas Stoeckert <Thomas@Stoeckert.us>
2021-05-31 14:34:18 -0400

    Fixed logout functionality

--- js/code.js
+++ js/code.js
@@ -208,5 +208,6 @@ function doLogout()
 	userData.firstName = "";
 	userData.lastName = "";
 	document.cookie = "userData= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+	
 	window.location.href = "index.html";
 }
