61f577495f8af9809e23a7fae652731c83b2d178
Timothy Golio <TimothyVGolio@gmail.com>
2021-06-07 11:43:15 -0400

    Adds comments

--- LAMPAPI/log_in.php
+++ LAMPAPI/log_in.php
@@ -1,12 +1,17 @@
 
 <?php
+	// COP4331 Group 15, 6/7/2021
+	// Creates an API endpoint that allows the user to log in to a pre-existing
+	// account.
 
 	$inData = getRequestInfo();
 
+	// Initializes the input values.
 	$ID = 0;
 	$FirstName = "";
 	$LastName = "";
 
+	// Establishes a connection with the mySQL database.
 	$conn = new mysqli("localhost", "Group15Admin", "ByVivec", "COP4331");
 	if( $conn->connect_error )
 	{
@@ -14,6 +19,8 @@
 	}
 	else
 	{
+		// Prepares and executes a mySQL statement that selects a user from the
+		// database with the inputted login and password.
 		$stmt = $conn->prepare("SELECT ID,FirstName,LastName FROM Users WHERE Login=? AND Password =?");
 		$stmt->bind_param("ss", $inData["Login"], $inData["Password"]);
 		$stmt->execute();
@@ -21,6 +28,7 @@
 
 		if( $row = $result->fetch_assoc() )
 		{
+			// Returns with the selected users info if they are found.
 			returnWithInfo( $row['FirstName'], $row['LastName'], $row['ID'], $row['Login'] );
 		}
 		else
@@ -45,12 +53,14 @@
 
 	function returnWithError( $err )
 	{
+		// Returns with the error in JSON.
 		$retValue = '{"ID":0,"FirstName":"","LastName":"","error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
 
 	function returnWithInfo( $FirstName, $LastName, $ID, $Login )
 	{
+		// Returns with the contact info in JSON.
 		$retValue = '{"ID":' . $ID . ',"FirstName":"' . $FirstName . '","LastName":"' . $LastName . '","Login":"' . $Login . '","error":""}';
 		sendResultInfoAsJson( $retValue );
 	}
