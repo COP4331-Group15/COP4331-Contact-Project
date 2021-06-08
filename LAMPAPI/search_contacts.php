3c3d723d3a3aa0e31f5984d9740351c53d274817
Thomas Stoeckert <Thomas@Stoeckert.us>
2021-06-08 13:29:49 -0400

    Fixed formatting issues from merge

--- LAMPAPI/search_contacts.php
+++ LAMPAPI/search_contacts.php
@@ -12,41 +12,31 @@
 
 	// Establishes a connection with the mySQL database.
 	$conn = new mysqli("localhost", "Group15Admin", "ByVivec", "COP4331");
-	if ($conn->connect_error)
-	{
+	if ($conn->connect_error) {
 		returnWithError($conn->connect_error);
-	}
-	else
-	{
+	} else {
 		// Prepares and executes a mySQL statement that selects all contacts with a
 		// similar first and last name and the given userID
-	$stmt = $conn->prepare("SELECT * FROM Contacts WHERE CONCAT_WS(' ', FirstName, LastName) LIKE ? AND (UserID = ?) ORDER BY LastName, FirstName");
-	$unifiedSearch = "%" . $inData["query"] . "%";
-	$stmt->bind_param("si", $unifiedSearch, $inData["userID"]);
-	$stmt->execute();
+		$stmt = $conn->prepare("SELECT * FROM Contacts WHERE CONCAT_WS(' ', FirstName, LastName) LIKE ? AND (UserID = ?) ORDER BY LastName, FirstName");
+		$unifiedSearch = "%" . $inData["query"] . "%";
+		$stmt->bind_param("si", $unifiedSearch, $inData["userID"]);
+		$stmt->execute();
 
-		if ($result = $stmt->get_result())
-		{
+		if ($result = $stmt->get_result()) {
 			// Loops through the resulting values of the mySQL statement and pushes them
 			// to an array.
-			while ($row = $result->fetch_assoc())
-			{
+			while ($row = $result->fetch_assoc()) {
 				$searchCount++;
 				array_push($searchResults, $row);
 			}
 
 			// Returns that values found or an error if no values are found.
-			if ($searchCount == 0)
-			{
+			if ($searchCount == 0) {
 				returnWithError("No results found.");
-			}
-			else
-			{
+			} else {
 				returnWithInfo(json_encode($searchResults));
 			}
-		}
-		else
-		{
+		} else {
 			returnWithError("Debug: Empty result set");
 		}
 
